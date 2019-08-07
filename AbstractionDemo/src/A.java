12d2dfdadcbe425553a0aaf3d230c5bc3d57e5f9
jbmiranda <jbmiranda@pagibigfund.gov.ph>
2019-08-07 14:53:05 +0800

    Updates

--- AbstractionDemo/src/A.java
+++ AbstractionDemo/src/A.java
@@ -1,6 +1,10 @@
 
 public class A {
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		A a = new A();
+	}*/
+	
+	public void aMethod() {
+		System.out.println("aMethod");
 	}
 }
